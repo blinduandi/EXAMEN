b162a4c8c8c111a0e6c6a40d8327fde8d373aad3
EusuntRA <67802124+EusuntRA@users.noreply.github.com>
2020-07-08 14:07:01 +0300

    zzzz

--- EXAMEN/Program.cs
+++ EXAMEN/Program.cs
@@ -9,25 +9,47 @@ namespace EXAMEN
         {
             Console.WriteLine("LOADING...");
         }
-
-
         public static void load()
         {
             //informatia din XML
         }
-
     }
-
     class Login
     {
-        bool rezultat; //o vom folosi pentru a memora daca utilizatorul are cont
-
+        //  bool rezultat; //o vom folosi pentru a memora daca utilizatorul are cont
+                 //string Log;
         public static void select()
         {
+
+            Console.WriteLine("\n\n\t\t\t\t\t╔═══════════════════════════════════════╗");
+            Console.WriteLine("\t\t\t\t\t║\t Autentificare in sistem \t║");
+            Console.WriteLine("\t\t\t\t\t╠═══════════════════════════════════════╣");
+            Console.WriteLine("\t\t\t\t\t║\t    Autentificare: 1\t\t║\n\t\t\t\t\t║\t    Inregistrare: 2\t\t║");
+            Console.WriteLine("\t\t\t\t\t╚═══════════════════════════════════════╝");
+
             //selectam cu ajutorul unui switch daca avem cont
+            switch (Console.ReadKey(true).KeyChar)
+            {
+                case '1':
+                    // daca logare
+                    Console.WriteLine("Logare");
+                    login();
+                    Console.Clear();
+                    break;
+                case  '2':
+                    // code block
+                    Console.WriteLine("Inreg");
+                    inregistrare();
+                    Console.Clear();
+                    break;
+
+            }
+
         }
         public static void login()
         {
+              //Log = Console.Read();
+
             //introducem parola si loginul
             //verificam daca datele sunt introduse corect
         }
@@ -68,6 +90,7 @@ namespace EXAMEN
 
         static void Main(string[] args)
         {
+            Login.select();
 
         }
     }
