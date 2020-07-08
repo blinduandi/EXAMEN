9cba26872dea1463fb879c27ea0f89c6ce0416d9
blinduandi <67801991+blinduandi@users.noreply.github.com>
2020-07-08 10:36:52 +0300

    HARTA
    SCHELET

--- EXAMEN/Program.cs
+++ EXAMEN/Program.cs
@@ -2,11 +2,73 @@
 
 namespace EXAMEN
 {
-    class Program
+    class Loading
     {
+
+        public static void show()
+        {
+            Console.WriteLine("LOADING...");
+        }
+
+
+        public static void load()
+        {
+         //informatia din XML
+        }
+
+    }
+
+    class Login
+    {
+        bool rezultat; //o vom folosi pentru a memora daca utilizatorul are cont
+
+            public static void select()
+        {
+         //selectam cu ajutorul unui switch daca avem cont
+        }
+            public static void login()
+        {
+         //introducem parola si loginul
+         //verificam daca datele sunt introduse corect
+        }    
+
+            public static void inregistrare()
+        {
+         //introducem parola, loginul, emailul, numele, prenumele, varsta 
+         //verificam daca datele sunt introduse corect
+        }        
+
+    }
+
+
+
+
+    class Principal
+    {
+
+            public static void pricipal()
+        {
+            //afisam informatia despre utilizatorul logat
+        }  
+
+
+            public static void search()
+        {
+            //afisam informatia despre utilizatorul cautat
+        }  
+
+
+            public static void prieteni()
+        {
+            //afisam prietenii utilizatoruli logat
+        }  
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
         }
     }
 }
